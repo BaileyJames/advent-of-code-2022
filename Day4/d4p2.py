f978a3330ca80aeeaf70c72dbe7c70bc5f752638
BaileyJames <baileyheyworth@gmail.com>
2022-12-04 16:00:45 +0000

    Add Day 4

---
+++ Day4/d4p2.py
@@ -0,0 +1,23 @@
+file = open("./input.txt", "r").read().split();
+
+assignments = []
+
+fullAssignments = []
+
+duplicateAssignment = 0
+
+for x in range(len(file)):
+    assignments.append(file[x].split(","))
+
+for x in range(len(assignments)):
+    firstElf = str(assignments[x][0]).split("-")
+    secondElf = str(assignments[x][1]).split("-")
+    
+    print(firstElf, secondElf)
+
+    if(int(firstElf[1]) >= int(secondElf[0]) and int(firstElf[0]) <= int(secondElf[0])):
+        duplicateAssignment += 1
+    elif(int(secondElf[1]) >= int(firstElf[0]) and int(secondElf[0]) <= int(firstElf[0])):
+        duplicateAssignment += 1
+
+print(duplicateAssignment)
